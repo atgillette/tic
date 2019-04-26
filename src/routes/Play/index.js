c3804dcde5e4bd6a5b831e2e21a5e985422603f1
Adrian Gillette <gillette@adrian.net>
2019-04-26 12:13:59 -0700

    Bug fix - Play container, add checkForWinner logic

--- src/routes/Play/index.js
+++ src/routes/Play/index.js
@@ -12,6 +12,45 @@ export const Play = ({ dispatch, game }) => {
   const [row, setRow] = useState('');
   const [column, setColumn] = useState('');
 
+  const winnerCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  const positionMap =  {
+    0: [0, 0],
+    1: [0, 1],
+    2: [0, 2],
+    3: [1, 0],
+    4: [1, 1],
+    5: [1, 2],
+    6: [2, 0],
+    7: [2, 1],
+    8: [2, 2],
+  };
+
+  const checkForWinner = (board) => {
+
+    for (let i = 0; i < winnerCombinations.length; i++) {
+      let [point1, point2, point3] = winnerCombinations[i];
+
+      point1 = positionMap[point1];
+      point2 = positionMap[point2];
+      point3 = positionMap[point3];
+
+      if (board[point1[0]][point1[1]] && board[point1[0]][point1[1]] === board[point2[0]][point2[1]] && board[point1[0]][point1[1]] === board[point3[0]][point3[1]]) {
+        return true;
+      }
+    }
+    return false;
+  };
+
   const submitMove = () => {
     if (row.trim() !== '' && !isNaN(row) && column.trim() !== '' && !isNaN(column)) {
       dispatch(move(Number(row), Number(column)));
@@ -19,6 +58,8 @@ export const Play = ({ dispatch, game }) => {
       alert('You need to pick a row and column before you can move!');
     }
   };
+
+
 
   // HACKY BOARD DRAWING
   const boardCellNumber = 3;
@@ -34,11 +75,25 @@ export const Play = ({ dispatch, game }) => {
   const rowHTML = [verticalBorder, ...board.map(drawRow), verticalBorder]
     .map((row, ind) => <p key={ ind }>{ row }</p>);
 
+  const hasWinner = checkForWinner(board);
+
   return (
+
     <CoreLayout className="game__container">
       <div>
         <h2>{ players[0] } vs. { players[1] }</h2>
-        <p>{ `${ players[game.currentPlayerIndex] }'s turn` }</p>
+
+        {
+          hasWinner
+              ? (
+                  <p>{ `${ players[(game.currentPlayerIndex + 1) % 2] } is the winner!!!` }</p>
+              )
+              : (
+                  <p>{ `${ players[game.currentPlayerIndex] }'s turn` }</p>
+              )
+        }
+
+
         <div>
           { rowHTML }
         </div>
@@ -76,6 +131,7 @@ export const Play = ({ dispatch, game }) => {
         <input
           className="submit"
           type="submit"
+          disabled={hasWinner}
           onClick={ () => { submitMove(); } }
         />
       </div>
